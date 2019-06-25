7a1bd7066811d3fd7170984b45d0c6efd6bfff69
sebah12 <hernandosebastian@gmail.com>
2019-06-25 19:24:59 -0300

    Fix agenda dir

--- init.el
+++ init.el
@@ -61,7 +61,7 @@
 (global-set-key (kbd "C-c b") 'org-iswitchb)
 
 ;; Agenda files
-(setq org-agenda-files '("~/org/agenda"))
+(setq org-agenda-files '("~/Dropbox/org/agenda/"))
 
 ;; TODO dependencies
 (add-hook 'org-mode-hook 'org-enforce-todo-dependencies)
@@ -145,17 +145,3 @@
 
 ;; init.el ends here
 ;; ------------------------------------------------------------
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (projectile which-key py-autopep8 material-theme magit flycheck elpy ein better-defaults))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
